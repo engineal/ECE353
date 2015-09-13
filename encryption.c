938661458385a2503229c2c16be5d5ecf014c0a0
engineAL <engineal@outlook.com>
2015-09-13 15:48:58 -0400

    Formatted Code

--- encryption.c
+++ encryption.c
@@ -15,85 +15,69 @@ void swap (char *, char *);
 char generateKeyByte (char []);
 
 int main(void) {
-
-    FILE *ifp;
-	FILE *ksf; 
-    FILE *ofp;
+    FILE *plainTextFile;
+    FILE *keyStreamFile;
+    FILE *encryptedTextFile;
     char inputChar;
     char outputChar;
-	char keyChar; 
-    int i;
-	int kLength;
-	char key [MAX_LENGTH];
-	char S [MAX_LENGTH];
-	char T[MAX_LENGTH];
+    char keyChar; 
+    int kLength = 0;
+    char key[MAX_LENGTH];
+    char S[MAX_LENGTH];
+    char T[MAX_LENGTH];
 
+    plainTextFile = fopen("./plainText.txt", "r");
+    keyStreamFile = fopen("./keyFile.txt", "r"); 
+    encryptedTextFile = fopen("./encryptedText.txt", "w");
 
+    assert(plainTextFile != NULL);
+    assert(keyStreamFile != NULL); 
+    assert(encryptedTextFile != NULL);
 
-    ifp = fopen("./plainText.txt", "r");
-	ksf = fopen("./keyFile.txt", "r"); 
-    ofp = fopen("./encryptedText.txt", "w");
+    // fill in key
+    while (((keyChar = fgetc(keyStreamFile)) != EOF) && kLength < MAX_LENGTH) {
+        key[kLength] = keyChar;
+        kLength++; 
+    }
 
-    assert(ifp != NULL);
-    assert(ofp != NULL);
-	assert(ifp != NULL); 
+    //fill in S, T
+    for (int i = 0; i < MAX_LENGTH; i++){
+        S[i] = i; 
+        T[i] = key[i % kLength];
+    }
 
-// fill in key
-kLength = 0;
- while (((keyChar = fgetc(ksf))!=EOF) && kLength<MAX_LENGTH) {
-	key[kLength] = keyChar;
-	kLength++; 
-}
+    //initial permutation
+    int j =0; 
+    for (int i =0; i< MAX_LENGTH; i++){
+        j = (j+S[i]+T[i]) % MAX_LENGTH; 
+        swap (&S[i], &S[j]);
+    }
 
-//fill in S, T
-for (int i = 0; i <MAX_LENGTH; i++){
-	S[i] = i; 
-	T[i] = key[i % kLength];
-}
+    //Part 3: for each character read, generate the next key stream elem. Xor key byte with read byte to form encrypted output. Write to output file.
+    while ((inputChar = fgetc(plainTextFile)) != EOF) {
+        keyChar = generateKeyByte(S);
+        outputChar = inputChar ^ keyChar;
+        fputc(outputChar, encryptedTextFile); 
+    }
 
-//initial permutation
-int j =0; 
-for (int i =0; i<MAX_LENGTH; i++){
-	j = (j+S[i]+T[i]) % MAX_LENGTH; 
-	swap (&S[i], &S[j]);
-}
-
-	
-
-
-
-
-
-//Part 3: for each character read, generate the next key stream elem. Xor key byte with read byte to form encrypted output. Write to output file.
-     while ((inputChar = fgetc(ifp))!=EOF) {
-	keyChar = generateKeyByte(S);
-      outputChar = inputChar ^ keyChar;
-       fputc(outputChar, ofp);
-      
-     }
-
-
-    fclose(ifp);
-    fclose(ofp);
-	fclose(ksf); 
-
+    fclose(plainTextFile);
+    fclose(keyStreamFile);
+    fclose(encryptedTextFile);
 }//end of main
+
 char generateKeyByte (char S[]){
-	static int i=0;
-	static int j=0;
-	i = (i+1) % MAX_LENGTH; 
-	j = (j+S[i]) % MAX_LENGTH; 
-	swap (&S[i], &S[j]);
-	int t = (S[i]+S[j]) % MAX_LENGTH; 
-	return S[t];
-
+    static int i = 0;
+    static int j = 0;
+    i = (i+1) % MAX_LENGTH; 
+    j = (j+S[i]) % MAX_LENGTH; 
+    swap (&S[i], &S[j]);
+    int t = (S[i]+S[j]) % MAX_LENGTH; 
+    return S[t];
 }
-
 
 void swap (char * a, char * b){
- char temp;
-   temp = *a;
-   *a = *b;
-   *b = temp;
+    char temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
-
