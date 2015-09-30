6d31435acf955793e8cb946fccb6df98aafdb07b
engineAL <engineal@outlook.com>
2015-09-30 18:30:09 -0400

    Runge-Kutta implemented
    Test code added

--- Project2/thermal.c
+++ Project2/thermal.c
@@ -9,11 +9,38 @@
 #include <math.h>
 #include <assert.h>
 
-double rk(int, int (*)(int, int));
+double test(double, double);
+double rk(double, double, double, double (*)(double, double));
 
 int main(int argc, char *argv[]) {
+    int i;
+    double h = 0.01;
+    double x = 0.0;
+    double y = 0.0;
+    for (i = 0; i < 100; i++) {
+        y = rk(h, x, y, &test);
+        x += h;
+        printf("x=%f, y=%f\n", x, y);
+    }
 }
 
-double rk(int h, int (*f)(int, int)) {
-    
+double test(double x, double y) {
+    return (x * x) + (y * y);
+}
+
+/**
+ * Runge-Kutta algorithm
+ *
+ * @param h step size
+ * @param x_n previous x
+ * @param y_n previous y
+ * @param f pointer to function
+ * @returns y(t+h)
+ */
+double rk(double h, double x_n, double y_n, double (*f)(double, double)) {
+    double k1 = h * f(x_n, y_n);
+    double k2 = h * f(x_n + h / 2, y_n + k1 / 2);
+    double k3 = h * f(x_n + h / 2, y_n + k2 / 2);
+    double k4 = h * f(x_n + h, y_n + k3);
+    return y_n + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
 }
