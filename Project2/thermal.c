13b37bf5a779972940b6d7cf3c38b7a1530fb335
engineAL <engineal@outlook.com>
2015-10-03 15:37:02 -0400

    Weird behavior with reading param file

--- Project2/thermal.c
+++ Project2/thermal.c
@@ -14,9 +14,9 @@ double *f(double, double[]);
 double *rk(double, double, double[]);
 void printParams();
 
-double w[4];
-double r[4][4];
 double c[4];
+double r[4][4];
+double w[4];
 
 int main(int argc, char *argv[]) {
     FILE *paramFile;
@@ -47,25 +47,27 @@ int main(int argc, char *argv[]) {
     
     char *line = malloc(sizeof(char) * 136);
     assert(fgets(line, 136, paramFile) != NULL);
-    double c1[4];
-    sscanf(line, "%lf %lf %lf %lf", &c1[0], &c1[1], &c1[2], &c1[3]);
-    printf("%f %f %f %f\n", &c1[0], &c1[1], &c1[2], &c1[3]);
     printf("%s", line);
+    assert(sscanf(line, "%lf%lf%lf%lf", &c[0], &c[1], &c[2], &c[3]) == 4);
+    printf("%f %f %f %f\n\n", &c[0], &c[1], &c[2], &c[3]);
     int i;
     for (i = 0; i < 4; i++) {
         assert(fgets(line, 136, paramFile) != NULL);
-        sscanf(line, "%lf %lf %lf %lf", &r[i][0], &r[i][1], &r[i][2], &r[i][3]);
         printf("%s", line);
+        assert(sscanf(line, "%lf%lf%lf%lf", &r[i][0], &r[i][1], &r[i][2], &r[i][3]) == 4);
+        printf("%f %f %f %f\n\n", &r[i][0], &r[i][1], &r[i][2], &r[i][3]);
     }
     
-    printParams();
+    //printParams();
     
     double x = 0.0;
     double y[] = {ambient, ambient, ambient, ambient};
     double prevt;
     while (fgets(line, 136, powerTraceFile) != NULL) {
         double t;
-        sscanf(line, "%lf %lf %lf %lf %lf", &t, &w[0], &w[1], &w[2], &w[3]);
+        printf("%s", line);
+        assert(sscanf(line, "%lf%lf%lf%lf%lf", &t, &w[0], &w[1], &w[2], &w[3]) == 5);
+        printf("%f %f %f %f %f\n\n", t, w[0], w[1], w[2], w[3]);
         double h = t - prevt;
         prevt = t;
         rk(h, x, y);
@@ -92,11 +94,11 @@ double *f(double x, double y[]) {
     for (i = 0; i < 4; i++) {
         for (j = 0; j < 4; j++) {
             if (i != j) {
-                *(result + i) -= (y[i] - y[j]) / (r[i][j] * c[i]);
+                result[i] -= (y[i] - y[j]) / (r[i][j] * c[i]);
             }
         }
         
-        *(result + i) += w[i] / c[i];
+        result[i] += w[i] / c[i];
     }
     
     return result;
@@ -114,25 +116,25 @@ double *rk(double h, double x_n, double y_n[]) {
     int i;
     double *k1 = f(x_n, y_n);
     for (i = 0; i < 4; i++) {
-        *(k1 + i) *= h;
+        k1[i] *= h;
     }
     
     double ya[4] = {y_n[0] + k1[0] / 2, y_n[1] + k1[1] / 2, y_n[2] + k1[2] / 2, y_n[3] + k1[3] / 2};
     double *k2 = f(x_n + h / 2, ya);
     for (i = 0; i < 4; i++) {
-        *(k2 + i) *= h;
+        k2[i] *= h;
     }
     
     double yb[4] = {y_n[0] + k2[0] / 2, y_n[1] + k2[1] / 2, y_n[2] + k2[2] / 2, y_n[3] + k2[3] / 2};
     double *k3 = f(x_n + h / 2, yb);
     for (i = 0; i < 4; i++) {
-        *(k3 + i) *= h;
+        k3[i] *= h;
     }
     
     double yc[4] = {y_n[0] + k3[0], y_n[1] + k3[1], y_n[2] + k3[2], y_n[3] + k3[3]};
     double *k4 = f(x_n + h, yc);
     for (i = 0; i < 4; i++) {
-        *(k4 + i) *= h;
+        k4[i] *= h;
     }
     
     for (i = 0; i < 4; i++) {
