03588eca34f9eff169b98f8524e8c4548f4aa5df
engineAL <engineal@outlook.com>
2015-10-07 17:26:43 -0400

    Calculating temperatures correctly

--- Project2/thermal.c
+++ Project2/thermal.c
@@ -10,13 +10,13 @@
 #include <assert.h>
 
 void usage(int argc, char *argv[]);
-double *f(double, double[]);
-double *rk(double, double, double[]);
+double *f(double, double[], double[]);
+double *rk(double, double[], double[]);
 void printParams();
 
+double h = 0.005;
 double c[4];
-double r[4][4];
-double w[4];
+double r[4][5];
 
 int main(int argc, char *argv[]) {
     FILE *paramFile;
@@ -48,24 +48,26 @@ int main(int argc, char *argv[]) {
     char *line = malloc(sizeof(char) * 136);
     assert(fgets(line, 136, paramFile) != NULL);
     assert(sscanf(line, "%lf%lf%lf%lf", &c[0], &c[1], &c[2], &c[3]) == 4);
-    int i;
-    for (i = 0; i < 4; i++) {
+    int j;
+    for (j = 0; j < 5; j++) {
         assert(fgets(line, 136, paramFile) != NULL);
-        assert(sscanf(line, "%lf%lf%lf%lf", &r[i][0], &r[i][1], &r[i][2], &r[i][3]) == 4);
+        assert(sscanf(line, "%lf%lf%lf%lf", &r[0][j], &r[1][j], &r[2][j], &r[3][j]) == 4);
     }
     
     printParams();
     
     double x = 0.0;
-    double y[] = {ambient, ambient, ambient, ambient};
-    double prevt;
+    double y[] = {ambient, ambient, ambient, ambient, ambient};
+    
     while (fgets(line, 136, powerTraceFile) != NULL) {
         double t;
+        double w[4];
         assert(sscanf(line, "%lf%lf%lf%lf%lf", &t, &w[0], &w[1], &w[2], &w[3]) == 5);
-        double h = t - prevt;
-        prevt = t;
-        rk(h, x, y);
-        x += h;
+        while (x < t) {
+            rk(x, y, w);
+            fprintf(outputFile, "%f %f %f %f\n", y[0], y[1], y[2], y[3]);
+            x += h;
+        }
     }
     
     free(line);
@@ -80,13 +82,15 @@ void usage(int argc, char *argv[]) {
     printf("%s paramFile powerFile optionalAmbientInput outputFile\n", argv[0]);
 }
 
-double *f(double x, double y[]) {
+double *f(double x, double y[], double w[]) {
     double *result = malloc(sizeof(double) * 4);
     
     int i;
     int j;
     for (i = 0; i < 4; i++) {
-        for (j = 0; j < 4; j++) {
+        result[i] = 0;
+        
+        for (j = 0; j < 5; j++) {
             if (i != j) {
                 result[i] -= (y[i] - y[j]) / (r[i][j] * c[i]);
             }
@@ -101,32 +105,31 @@ double *f(double x, double y[]) {
 /**
  * Runge-Kutta algorithm
  *
- * @param h step size
  * @param x_n previous x
  * @param y_n previous ys
  * @returns y(t+h)
  */
-double *rk(double h, double x_n, double y_n[]) {
+double *rk(double x_n, double y_n[], double w[]) {
     int i;
-    double *k1 = f(x_n, y_n);
+    double *k1 = f(x_n, y_n, w);
     for (i = 0; i < 4; i++) {
         k1[i] *= h;
     }
     
-    double ya[4] = {y_n[0] + k1[0] / 2, y_n[1] + k1[1] / 2, y_n[2] + k1[2] / 2, y_n[3] + k1[3] / 2};
-    double *k2 = f(x_n + h / 2, ya);
+    double ya[] = {y_n[0] + k1[0] / 2, y_n[1] + k1[1] / 2, y_n[2] + k1[2] / 2, y_n[3] + k1[3] / 2, y_n[4]};
+    double *k2 = f(x_n + h / 2, ya, w);
     for (i = 0; i < 4; i++) {
         k2[i] *= h;
     }
     
-    double yb[4] = {y_n[0] + k2[0] / 2, y_n[1] + k2[1] / 2, y_n[2] + k2[2] / 2, y_n[3] + k2[3] / 2};
-    double *k3 = f(x_n + h / 2, yb);
+    double yb[] = {y_n[0] + k2[0] / 2, y_n[1] + k2[1] / 2, y_n[2] + k2[2] / 2, y_n[3] + k2[3] / 2, y_n[4]};
+    double *k3 = f(x_n + h / 2, yb, w);
     for (i = 0; i < 4; i++) {
         k3[i] *= h;
     }
     
-    double yc[4] = {y_n[0] + k3[0], y_n[1] + k3[1], y_n[2] + k3[2], y_n[3] + k3[3]};
-    double *k4 = f(x_n + h, yc);
+    double yc[] = {y_n[0] + k3[0], y_n[1] + k3[1], y_n[2] + k3[2], y_n[3] + k3[3], y_n[4]};
+    double *k4 = f(x_n + h, yc, w);
     for (i = 0; i < 4; i++) {
         k4[i] *= h;
     }
@@ -144,9 +147,10 @@ double *rk(double h, double x_n, double y_n[]) {
 }
 
 void printParams() {
+    printf("h: %f\n", h);
     printf("C: %f %f %f %f\n", c[0], c[1], c[2], c[3]);
-    int i;
-    for (i = 0; i < 4; i++) {
-        printf("R: %f %f %f %f\n", r[i][0], r[i][1], r[i][2], r[i][3]);
+    int j;
+    for (j = 0; j < 5; j++) {
+        printf("R%d: %f %f %f %f\n", j, r[0][j], r[1][j], r[2][j], r[3][j]);
     }
 }
