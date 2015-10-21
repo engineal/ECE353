41beae4bb479b05c8cb55acbc1575c4f93811f59
engineAL <engineal@outlook.com>
2015-10-21 19:31:14 -0400

    Fixed memory leak

--- Project3/clock.c
+++ Project3/clock.c
@@ -7,8 +7,13 @@ struct LatchD *stateD = malloc(sizeof(struct LatchD));
 int running;
 while (running) {
     writeBack(stateD);
+    free(stateD);
     stateD = memory(stateC);
+    free(stateC);
     stateC = execute(stateB);
+    free(stateB);
     stateB = instructionDecode(stateA);
+    free(stateA);
     stateA = instructionFetch(PC);
+    PC++;
 }
