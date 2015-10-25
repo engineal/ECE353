f6dc9de0570a15f14e005d623484c4a6148281ec
Matteo95 <mpuzella@umass.edu>
2015-10-25 18:03:58 -0400

    final

--- Project3/stages.c
+++ Project3/stages.c
@@ -5,3 +5,11 @@ struct Instruction instructions[];
 void IF{
 LatchA.instruction = instructions[pc];
 }
+
+//WB Stage
+void WB {
+registers[LatchD.rd] =LatchD.result ;
+}
+
+//struct Register Registers[32]; 
+
