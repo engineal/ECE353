a75d392aba64cc9f11b639cedfde4b05f6ad80b5
engineAL <engineal@outlook.com>
2015-10-25 18:02:33 -0400

    Cleaned up merge

--- Project3/stages.c
+++ Project3/stages.c
@@ -1,3 +1,8 @@
+//Array of registers
+struct Register registers[32];
+//Data memory
+int *dataMem = malloc(sizeof(int) * 64);
+
 // IF
 
 // ID
@@ -14,32 +19,20 @@ void ID(){
 		LatchB.reg2 = 0;  
 		
 		// Set LatchA ready value to…..? 
-		LatchA.ready = true; 
-
+		LatchA.ready = true;
 	}
 	//flag == true, registers safe
 	else{
 		LatchB.opcode = LatchA.instruction.opcode; 
 		LatchB.rd = LatchA.instruction.rd; //pass reg #
-		LatchB.reg1 = Registers[LatchA.instruction.rs]; //pass reg value
-		LatchB.reg2 =  Registers[LatchA.instruction.rt]; //pass reg value
+		LatchB.reg1 = registers[LatchA.instruction.rs]; //pass reg value
+		LatchB.reg2 =  registers[LatchA.instruction.rt]; //pass reg value
 
 		LatchB.ready = true;  
-	}
-
-	
-}
-// EXE
+	}	
 }
 
-//Array of registers
-struct Register Registers[32]; 
-
-// MEM
-
-// WB
-
-//takes in register string returns register number, to be used as index for Registers[] array
+//takes in register string returns register number, to be used as index for registers[] array
 int RegisterStringtoInt(char *s){
 	s =tolower(s); 
 	char* regNames = ["zero", "at", "v0", "v1", "a0", "a1", "a2", "a3", "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"]; 
@@ -59,18 +52,6 @@ int RegisterStringtoInt(char *s){
 
 	/* to do : what if the register in invalid? ie out of 0-31 or a word etc*/	
 }
-
-	//make sure regs is within 
-	assert(reg<32); 
-	assert(reg<=0); 
-
-	return reg; 
-
-	/* to do : what if the register in invalid? ie out of 0-31 or a word etc*/	
-}
-
-
-
 
 // EXE
 struct LatchC *execute(struct LatchB *state) {
@@ -100,8 +81,6 @@ struct LatchC *execute(struct LatchB *state) {
     
     return result;
 }
-
-int *dataMem = malloc(sizeof(int) * 64);
 
 // MEM
 struct LatchD *memory(struct LatchC *state) {
