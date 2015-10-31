aa215120997481756e071b6185d1ae1aacb6f9a5
Matteo95 <mpuzella@umass.edu>
2015-10-31 13:47:20 -0400

    array_to_instruction

--- Project3/test.c
+++ Project3/test.c
@@ -4,10 +4,14 @@
 #include <string.h>
 #include <math.h>
 #include <assert.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 #include "sim-mips.c"
 
 bool RegisterStringtoIntTest(void);
+bool ArraytoInstructionTest(void);
 
 int main(int argc, char *argv[]) {
     bool result = true;
@@ -31,6 +35,44 @@ bool RegisterStringtoIntTest(void) {
     int i;
     for (i = 0; i < 1; i++) {
         pass &= (result[i] == RegisterStringtoInt(input[i]));
+    }
+    
+    return pass;
+}
+
+bool ArraytoInstructionTest(void) {
+    char *input[7];
+    struct Instruction result[7];
+
+    input[0] = "add $s0 $s1 $t0";
+    result[0].opcode = add;
+
+    input[1] = "sub $s0 $s1 $t0";
+    result[1].opcode = sub;
+    
+    input[2] = "mul $s0 $s1 $t0";
+    result[2].opcode = mul;
+
+    srand(time(NULL));
+    int r = rand();
+
+    input[3] = "addi $s0 $s1 r";
+    result[3].opcode = addi;
+
+    input[4] = "lw $s0 $s1 r";
+    result[4].opcode = lw;
+
+    input[5] = "sw $s0 $s1 r";
+    result[5].opcode = sw;
+
+    input[6] = "beq $s0 $s1 r";
+    result[6].opcode = beq;
+
+    bool pass = true;
+    
+    int i;
+    for (i = 0; i < 1; i++) {
+        pass &= (result[i] == arraytoInstruction(input[i]));
     }
     
     return pass;
